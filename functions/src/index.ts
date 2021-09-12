8838a8ab0e735cb348e89bf424162d8538e3b56c
Vijay <vijayj691@gmail.com>
2021-09-12 22:16:21 +0800

    get tokens data from firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,35 +12,38 @@ exports.sendeMail = functions.https.onRequest(async (request, response) => {
   
     let data:any = request.body;
     console.log(data);
-
-    var transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            type: 'OAuth2',
-            user: 'vjtask1@gmail.com',
-            clientId: '223245186845-ie1abch9fr4v6qi0r8tlt5idh73d7lgs.apps.googleusercontent.com',
-            clientSecret: 'oKj_Ndm0O8QCpuu6C2n2mjZ0',
-            refreshToken: '1//04CbKoNK9zXy6CgYIARAAGAQSNwF-L9IrHfMDFBM15q9C2pehMcHcVpE-9KL8lrFJS9bzmcGFO5QMK0ZBSj37OrvzSY5XlwMmRmc',
-            accessToken: 'ya29.a0ARrdaM_n_UDlyD184jJzkAjrwdvgE_Jrjjal3OntZu5avvQp0il_QA6rCc1CjQqJZeZSXaBiwXp3zXxy1QYxZP5x7KfheOM7pqoPaoaT15iScYflSu2jwX6EHD3Z6b39fOallS6gXc2B0YL9fcQh9432NBkm'
+    await admin.firestore().collection("Google Data").get()
+    .then(userData => {
+        let userDetails = userData.docs[0].data();
+        var transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                type: 'OAuth2',
+                user: userDetails.user,
+                clientId: userDetails.clientId,
+                clientSecret: userDetails.clientSecret,
+                refreshToken: userDetails.refreshToken,
+                accessToken: userDetails.accessToken
+            }
+        });
+        
+        var mail = {
+            from: userDetails.user,
+            to: data.email_to,
+            subject: data.subject,
+            text: data.body
         }
-    });
-    
-    var mail = {
-        from: "vjtask1@gmail.com",
-        to: data.email_to,
-        subject: data.subject,
-        text: data.body
-    }
-    
-    transporter.sendMail(mail, function(err, info) {
-        if (err) {
-            response.send("Error: " + JSON.stringify(err));
-        } else {
-            response.send("Success: " + JSON.stringify(info));
-        }
-        transporter.close();
+        
+        return transporter.sendMail(mail, function(err, info) {
+            if (err) {
+                response.send("Error: " + JSON.stringify(err));
+            } else {
+                response.send("Success: " + JSON.stringify(info));
+            }
+            transporter.close();
+        });
     });
 
 });
